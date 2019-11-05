2e9b62aa22cf71c3b32155d91763965d2392ba5b
Brittany Houtz <brittany.houtz@gmail.com>
2019-11-05 12:22:21 -0800

    API data displaying

--- pokedex/PokemonItem.js
+++ pokedex/PokemonItem.js
@@ -2,24 +2,26 @@ import Component from '../Component.js';
 
 class PokemonItem extends Component {
     renderHTML() {
-        // const pokemon = this.props.pokemon;
+        const { pokemon: {
+            url_image,
+            id,
+            pokemon,
+            type_1,
+            type_2,
+            weight,
+        },
+        } = this.props;
+
         return /*html*/`
 
             <div class="pokemon-item">
-                <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png">
-                <h2>#001 ・ Bulbasaur</h2>
-                <p>Type: Grass + Poison</p>
-                <p>Weight: 15.2 lbs</p>
+                <img src="${url_image}">
+                <h2>#${id} ・ ${pokemon}</h2>
+                <p>Type: ${type_1} + ${type_2}</p>
+                <p>Weight: ${weight} lbs</p>
             </div>
         `;
     }
 }
 
 export default PokemonItem;
-
-// <!-- <div class="pokemon-item">
-// <img src="${pokemon.Image}">
-// <h2>#${pokemon.Number} ・ ${pokemon.Name}</h2>
-// <p>Type: ${pokemon.Type}</p>
-// <p>Weight: ${pokemon.Weight}</p>
-// </div> -->
